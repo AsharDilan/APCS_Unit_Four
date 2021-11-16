365eabcb58b861c85e9ebecc673f4ecd4c84c39f
AsharDilan <ashardi@ssfs.org>
2021-11-16 15:06:56 -0500

    11/16/21 Could you take a look at this? I do know why there are so many errors. I also know that I am not using a lot of loops, so I can redo it if you want me to.

--- src/Disemvoweling.java
+++ src/Disemvoweling.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Disemvoweling {
-}
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the disemvoweling utility.");
+        System.out.println("Enter Your Phrase:");
+        String s = scan.next();
+    }
+    public String disemvowel(String s) {
+        boolean vowel(char c);
+        if (c == 'a') {
+            return true;
+        else if (c == 'e') {
+            return true;
+        }
+        else if (c == 'i') {
+            return true;
+        }
+        else if (c == 'o') {
+            return true;
+        }
+        else if (c == 'u') {
+            return true;
+            }
+        }
+        String noVowel = " ";
+        int l = s.length();
+        for (int m = 0; m<=1;m++) {
+
+            if (vowel == "false"){
+                char n = s.charAt(m);
+                noVowel = noVowel + l;
+            }
+        }
+        return noVowel;
+    }
