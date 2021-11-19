194cd69bcac3d70e471e9f0f45d3595b61e66ce1
AsharDilan <ashardi@ssfs.org>
2021-11-18 21:15:51 -0500

    11/18/21 Could you take a look at this edited version? I tried to solve some errors, but I still cannot run the program in order to test it.

--- src/Disemvoweling.java
+++ src/Disemvoweling.java
@@ -6,32 +6,32 @@ public class Disemvoweling {
         System.out.println("Welcome to the disemvoweling utility.");
         System.out.println("Enter Your Phrase:");
         String s = scan.next();
+
     }
-    public String disemvowel(String s) {
-        boolean vowel(char c);
+    public boolean disemvowel(String s) {
+        boolean isVowel(char c);
         if (c == 'a') {
             return true;
         else if (c == 'e') {
-            return true;
-        }
-        else if (c == 'i') {
-            return true;
-        }
-        else if (c == 'o') {
-            return true;
-        }
-        else if (c == 'u') {
-            return true;
+                return true;
+            } else if (c == 'i') {
+                return true;
+            } else if (c == 'o') {
+                return true;
+            } else if (c == 'u') {
+                return true;
             }
+            return false;
         }
         String noVowel = " ";
         int l = s.length();
-        for (int m = 0; m<=1;m++) {
-
-            if (vowel == "false"){
-                char n = s.charAt(m);
-                noVowel = noVowel + l;
+        for (int m = 0; m <= 1; m++) {
+            boolean yesVowel = isVowel(s.charAt(m));
+            if (isVowel == "false") {
+                char x = s.charAt(m);
+                noVowel = noVowel + x;
             }
         }
         return noVowel;
+        System.out.println(noVowel);
     }
